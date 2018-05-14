210d4ead9a98a5e8d9f85375a8cc97b04fca6070
VDenysov <vdenysov@lulusoftware.com>
2018-05-14 12:14:51 +0300

    Pages refactored

--- Manicure.Web/Controllers/HomeController.cs
+++ Manicure.Web/Controllers/HomeController.cs
@@ -1,17 +1,9 @@
 ﻿using System.Web.Mvc;
-using Manicure.BusinessLogic.Authentication;
 
 namespace Manicure.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IAuthProvider _authProvider;
-
-        public HomeController(IAuthProvider authProvider)
-        {
-            _authProvider = authProvider;
-        }
-
         [Route("")]
         public ActionResult Main()
         {
---
+++ Manicure.Web/Models/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Manicure.Web.Models
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Photo { get; set; }
+
+        public string Role { get; set; }
+    }
+}
