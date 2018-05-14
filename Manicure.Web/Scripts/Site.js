fe7c8e73422220820a30244623ecc907d0f9879e
GKulabuhov <gkulabuhov@lulusoftware.com>
2018-05-14 12:15:52 +0300

    add new styles

--- Manicure.Web/Scripts/Site.js
+++ Manicure.Web/Scripts/Site.js
@@ -5,8 +5,8 @@
         e.preventDefault();
         $(".addField").slideToggle('slow');
     });
-
-    $('.gallery').slick({
+ 
+    $('.gallery-main').slick({
         dots: true,
         infinite: true,
         speed: 300,
@@ -16,6 +16,43 @@
         autoplay: true,
         autoplaySpeed: 3500,
         slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.gallery-masters').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        centerMode: true,
+        variableWidth: true,
+        adaptiveHeight: true,
+        autoplay: false,
+        slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
             {
